c3f1dcfa842419d004e692343365a06d4b26d107
Hauke <his@gmx.de>
2022-03-01 10:13:50 +0100

    vowels

--- stats.py
+++ stats.py
@@ -7,8 +7,10 @@ for x in f:
 
 words_with_doubles = []
 words_with_double_sequence = []
+words_with_double_vowels = []
 
 regexp = re.compile(r"(.)\1")
+regexvowel = re.compile(r"([aeiuo])\1")
 
 for w in words:
     match = re.search(regexp,w)
@@ -18,6 +20,12 @@ for w in words:
     match = re.search(regexp,sorted_w)
     if match:
         words_with_doubles.append(w)
+        matchvow = re.search(regexvowel, sorted_w)
+        if match:
+            match = re.search(regexp, w)
+            if not match:
+                words_with_double_vowels.append(w)
 
 print ("Words with double letters in sequence: ", len(words_with_double_sequence), " out of ", len(words))
 print ("Words with double letters anywhere: ", len(words_with_doubles), " out of ", len(words))
+print ("Words with double vowels: ", len(words_with_double_vowels), " out of ", len(words))
